7a182a3871f3e9669c013550775f40234709841f
lixin4ever <lixin4ever@gmail.com>
2016-08-17 00:48:01 +0800

    create the basic class of the data point (samples)

---
+++ data_point/data_point.py
@@ -0,0 +1,14 @@
+__author__ = 'lixin77'
+
+import numpy as np
+
+class Sample:
+    def __init__(self, features, label, data_type='float64'):
+        """
+        constructor of data point
+        :param features: feature value of the input samples
+        :param label: label / ground truth of the input samples
+        :param data_type: value type of the features, float64 is default setting
+        """
+        self.X = np.arrays(features, dtype=data_type)
+        self.Y = label
