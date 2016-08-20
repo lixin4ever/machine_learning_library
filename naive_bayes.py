03a54c145f31a5f3a7a69f607c0aea8a396d48e8
lixin4ever <lixin4ever@gmail.com>
2016-08-20 14:14:34 +0800

    complete first edition of multinomial naive bayes and testing module

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,7 +1,7 @@
 __author__ = 'lixin77'
 
 from preprocess import *
-import cmath
+import math
 import numpy as np
 
 # naive bayes models
@@ -9,51 +9,50 @@ class NB(object):
     pass
 
 class multinomial_NB(object):
-    def __init__(self, use_prior=True):
+    def __init__(self, use_prior=True, alpha=0.01):
         self.use_prior = use_prior
-
-    def set_prior(self, alpha=0):
         self.alpha = alpha
 
-    def train(self, texts, labels):
+    def train(self, X, Y, vocab):
         """
         train the multinomial NB model although NB does not have a explicit training process
-        :param texts: text in the training set
-        :param labels: labels of the training documents
+        :param X: sparse representation of the sentence-term matrix
+        :param Y: ground truth label of each sentence
+        :param cv: count vectorizor based on the statics of training set
         """
-        count_vectorizor, sparse_feature_mat = get_word_count(sentences=texts)
-        self.vocab = count_vectorizor.vocabulary_
+        #count_vectorizor, _, sparse_feature_mat = build_dataset(sentences=texts)
+        self.vocab = vocab
         self.indexed_vocab = {}
         for (k, v) in self.vocab.iteritems():
             self.indexed_vocab[v] = k
-        # analyser can perform pre-processing and tokenization
-        self.analyser = count_vectorizor.build_analyser()
-        self.class_count, self.class_count_summary = np.zeros(len(labels)), {}
+        # self.class_count ==>> number of training samples in each class
+        # self.class_count_summary ==>> word count in each class
+        self.class_count, self.class_count_summary = np.zeros(len(Y)), {}
         # number of training samples
-        self.n_train = len(texts)
-        for y in labels:
+        self.n_train = len(X)
+        for y in Y:
             self.class_count[y] = 0
             self.class_count_summary[y] = {}
-        for (doc, y) in zip(sparse_feature_mat, labels):
+        for (sen, y) in zip(X, Y):
             self.class_count[y] += 1
-            for w in doc:
-                count = doc[w]
+            for wid in sen:
+                count = sen[wid]
                 try:
-                    self.class_count_summary[y][w] += count
+                    self.class_count_summary[y][wid] += count
                 except KeyError:
-                    self.class_count_summary[y][w] = count
+                    self.class_count_summary[y][wid] = count
 
-    def predict(self, texts):
+    def predict(self, X):
         """
         predict labels of the testing documents and return the probabilities over labels for each document
-        :param texts: testing sentences or documents
+        :param X: sparse form of testing sentences matrix
         """
-        clear_texts = [preprocess(sentence=text) for text in texts]
         labels = self.class_count_summary.keys()
         # score is a 2d matrix recording probability of the event that document is assigned to one label
-        score = np.zeros(len(clear_texts), len(labels))
+        score = np.zeros((len(X), len(labels)))
         # avoid some class(es) is empty
-        p_y = float(self.class_count + self.alpha) / (self.n_train + len(self.class_count) * self.alpha)
+        p_y = [float(self.class_count[i] + self.alpha) / (self.n_train + len(self.class_count) * self.alpha)
+               for i in xrange(len(self.class_count))]
         p_x_y = {}
         for y in labels:
             p_x_y[y] = {}
@@ -63,19 +62,21 @@ class multinomial_NB(object):
                 word_count += self.class_count_summary[y][w]
             for w in self.class_count_summary[y]:
                 p_x_y[y][w] = (self.class_count_summary[y][w] + self.alpha) / (word_count + len(self.vocab) * self.alpha)
-        for i in xrange(len(clear_texts)):
-            text = clear_texts[i]
-            words = self.analyser(text)
+        for i in xrange(len(X)):
+            sen = X[i]
+            # pre-process and tokenize the input testing text
             for y in labels:
-                score[i][y] += cmath.log(p_y[y])
-                for j in xrange(words):
-                    w = words[j]
+                score[i][y] += math.log(p_y[y])
+                for j in sen:
+                    wid = j
+                    # times ==>> times that word occurs in current sentence
+                    times = sen[j]
                     try:
-                        score[i][y] += cmath.log(p_x_y[y][w])
+                        score[i][y] += (times * math.log(p_x_y[y][wid]))
                     except KeyError:
                         # new word that does not occur in the training set
                         p_new = self.alpha / (word_count + len(self.vocab) * self.alpha)
-                        score[i][y] += cmath.log(p_new)
+                        score[i][y] += (times * math.log(p_new))
         return score, np.argmax(score, axis=1)
 
 class bernoulli_NB(object):
--- test_NB.py
+++ test_NB.py
@@ -3,7 +3,18 @@ __author__ = 'lixin77'
 from sklearn.naive_bayes import MultinomialNB
 from preprocess import *
 import random
+from naive_bayes import multinomial_NB
+import numpy as np
+import time
 
+
+def compute_accu(Y_gold, Y_pred):
+    assert len(Y_gold) == len(Y_pred)
+    hit_count = 0
+    for i in xrange(len(Y_gold)):
+        if Y_gold[i] == Y_pred[i]:
+            hit_count += 1
+    return float(hit_count) / len(Y_gold)
 
 dataset_name = 'MR'
 
@@ -15,32 +26,71 @@ with open('dataset/%s/%s.txt' % (dataset_name, dataset_name)) as fp:
     for line in fp:
         label, text = line.strip().split('\t')
         try:
-            data[label].append(line.strip())
+            data[int(label)].append(line.strip())
         except KeyError:
-            data[label] = [line.strip()]
+            data[int(label)] = [line.strip()]
         n_samples += 1
 
-fold = 10
+N = 10
+accu_mnb_sklearn = 0.0
+accu_mnb_manual = 0.0
 
-for i in xrange(fold):
-    X_train = []
-    X_test = []
+for y in data:
+    random.shuffle(data[y])
+
+
+for i in xrange(N):
+    print "in the round %s..." % i
+    train_sen = []
+    test_sen = []
     Y_train = []
     Y_test = []
-    train_count = int(n_samples * 0.9)
-    ctr = 0
+    # stratified sampling, to ensure distribution of each class will not change after the split
     for y in data:
-        random.shuffle(data[y])
-        for sample in data[y]:
-            y, x = sample.split('\t')
-            if ctr < train_count:
-                X_train.append(x)
-                Y_train.append(y)
+        # split dataset as 10 subset
+        single_fold = int(len(data[y]) * 0.1)
+        for j in xrange(len(data[y])):
+            label, x = data[y][j].split('\t')
+            if i * single_fold <= j < (i + 1) * single_fold:
+                Y_test.append(int(label))
+                test_sen.append(x)
             else:
-                X_test.append(x)
-                Y_test.append(y)
-            ctr += 1
+                Y_train.append(int(label))
+                train_sen.append(x)
+    print 'number of training samples', len(train_sen)
+    print 'number of testing samples', len(test_sen)
+    Y_test = np.array(Y_test)
+    print "build the dataset..."
+    start_time = time.time()
+    # build the training set
+    X_train, X_train_sparse, X_test, X_test_sparse, vocab = build_dataset(train_sen=train_sen, test_sen=test_sen)
+    print 'time cost of processing the data: %s seconds' % (time.time() - start_time)
+    print "run the model in sklearn....."
+    start_time = time.time()
+    mnb_sklearn = MultinomialNB(alpha=0.01)
+    mnb_sklearn.fit(X_train, Y_train)
+    Y_pred_mnb_sklearn = mnb_sklearn.predict(X_test)
+    print 'time cost of sklearn: %s seconds' % (time.time() - start_time)
+    assert Y_pred_mnb_sklearn.shape[0] == len(Y_test)
+    perf_mnb_sklearn = compute_accu(Y_gold=Y_test, Y_pred=Y_pred_mnb_sklearn)
+    accu_mnb_sklearn += perf_mnb_sklearn
 
+    print "run the manually implemented model..."
+    start_time = time.time()
+    mnb_manual = multinomial_NB(use_prior=True, alpha=0.01)
+    mnb_manual.train(X=X_train_sparse, Y=Y_train, vocab=vocab)
+    p_Y_X, Y_pred_mnb_manual = mnb_manual.predict(X=X_test_sparse)
+    print 'time cost of manual implementation: %s seconds' % (time.time() - start_time)
+    assert Y_pred_mnb_manual.shape[0] == len(Y_test)
+    perf_mnb_manual = compute_accu(Y_gold=Y_test, Y_pred=Y_pred_mnb_manual)
+    accu_mnb_manual += perf_mnb_manual
+    print "In round %s, sklearn: %s, manual: %s" % (i, perf_mnb_sklearn, perf_mnb_manual)
+
+accu_mnb_manual /= N
+accu_mnb_sklearn /= N
+
+print "performance of multinomial NB model in sklearn is %s%%" % (100 * accu_mnb_sklearn)
+print "performance of multinomial NB model implemented by myself is %s%%" % (100 * accu_mnb_manual)
 
 
 
