f61764399e69fe28c121f63b717884ee1e9637db
lixin4ever <lixin4ever@gmail.com>
2016-08-28 23:12:08 +0800

    re-organize the testing module of naive bayes

--- test_NB.py
+++ test_NB.py
@@ -7,15 +7,8 @@ from sklearn.naive_bayes import MultinomialNB, BernoulliNB, GaussianNB
 from preprocess import *
 from model.naive_bayes import multinomial_NB, bernoulli_NB, gaussian_NB
 import numpy as np
+from test.utils import *
 
-
-def compute_accu(Y_gold, Y_pred):
-    assert len(Y_gold) == len(Y_pred)
-    hit_count = 0
-    for i in xrange(len(Y_gold)):
-        if Y_gold[i] == Y_pred[i]:
-            hit_count += 1
-    return float(hit_count) / len(Y_gold)
 
 dataset_name = 'CR'
 
